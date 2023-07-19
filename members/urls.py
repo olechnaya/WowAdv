ea9541259acfda8a8061531cdd12820e2babff5b
Olga <data19101988@gmail.com>
2023-07-19 11:53:56 +0300

    rename signup

--- members/urls.py
+++ members/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from allauth.account.views import LoginView, SignupView,LogoutView
+from allauth.account.views import LoginView, SignupView
 # from theWowAdv.views import
 
-# app_name="wow_members"
+app_name="wow_members"
 
 urlpatterns = [
-    # path('login',LoginView.as_view(), name="custom_login" ),
-    # path('signup', SignupView.as_view(), name="custom_singup" ),
-    # path('logout', LogoutView.as_view(), name="custom_logout" ),
+    path('login',LoginView.as_view(), name="custom_login" ),
+    path('signup', SignupView.as_view(), name="custom_signup" ),
     # path('<int:pk>', views.profile, name="index"),
 ]
